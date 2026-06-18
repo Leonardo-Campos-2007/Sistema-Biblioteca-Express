551d0e8399debbc57f8c7d6b129e7cb78a1017e3
Heitor Alves <heitoralvesreboucas@gmail.com>
2026-06-18 10:14:08 -0300

    Atualizar o db.js
    Removendo as anotações

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 
-//parametros de configuração do banco (credenciais)
+
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -9,7 +9,6 @@ const db = mysql.createConnection({
     port: '3306' 
 });
 
-//estabelecer a conexão
 db.connect(err =>{
     if(err) throw err;
     console.log('conectado ao banco de dados');
